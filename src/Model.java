b5a62c92504114ce8b737744ec2a8091e63fa751
edono <emd5310@rit.edu>
2021-02-18 21:39:30 -0500

    Model.java now takes a 2D array of strings holding the tables contents and builds a table holding those values. Controller.java has been edited accordingly and had comments revised

--- src/Model.java
+++ src/Model.java
@@ -1,3 +1,46 @@
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+
 public class Model {
 
+    public String writeLatex(String[][] tableContents, int rows, int cols){
+
+        // Line across top of table
+        String header = "\\begin{center}\n\\begin{tabular}{ " + "|c".repeat(cols) + "| }\n" + "\\hline\n";
+
+        StringBuilder body = new StringBuilder();
+
+        for(int i = 0; i < cols; i++){
+
+            StringBuilder newLine = new StringBuilder();
+
+            for(int j = 0; j < rows; j++){
+                newLine.append(tableContents[i][j]);
+                if(j == (rows - 1)){
+                    newLine.append(" \\\\\n");
+                    newLine.append("\\hline\n");
+                }
+                else{
+                    newLine.append(" & ");
+                }
+            }
+            body.append(newLine);
+        }
+
+        // Line across bottom of table
+        String footer = """
+                \\end{tabular}
+                \\end{center}""";
+
+        String table = header + body.toString() + footer;
+        System.out.println("[LaTeX TableMaker] LaTeX written");
+        return table;
+    }
+
+    public void exportLatex(String filepath, String filename, String table){
+        // Create the file of filename @ filepath
+        System.out.println("[LaTeX TableMaker] Exported!");
+    }
 }
